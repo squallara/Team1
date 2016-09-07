01c20dfa461e08890becfddf839c513b3a2e8cc2
Alexandros Panagiotis Giakalis <agiaka15@student.aau.dk>
2016-09-07 21:04:50 +0200

    And another one for testing

---
+++ Team1 Project/Assets/IhopeFinal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IhopeFinal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
