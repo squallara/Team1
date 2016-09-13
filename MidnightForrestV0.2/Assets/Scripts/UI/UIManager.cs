9e5e86a75b470409383ba1fc5c01959dc3f98cef
Nicolaj <nicolajevers@hotmail.com>
2016-09-13 12:13:47 +0200

    no message

--- MidnightForrestV0.2/Assets/Scripts/UI/UIManager.cs
+++ MidnightForrestV0.2/Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
-    public Button button;
-    public static UIManager instance = null;
+    public Canvas GUI_canvas;
+    public GameObject Instruction;
 
     Canvas UI_canvas;
 
@@ -13,36 +14,39 @@ public class UIManager : MonoBehaviour {
     {
         UI_canvas = GetComponent<Canvas>();
         UI_canvas.enabled = false;
-    }
-
-    void Update()
-    {
-        if(button == true)
-        {
-            OpenMenu();
-        }
+        Instruction.SetActive(false);
+        GUI_canvas.enabled = true;
     }
 
     public void OpenMenu()
     {
         UI_canvas.enabled = true;
         Time.timeScale = 0;
+        GUI_canvas.enabled = false;
     }
 
     public void Resume()
     {
         UI_canvas.enabled = false;
         Time.timeScale = 1;
+        GUI_canvas.enabled = true;
     }
 
-    public void Options()
+    public void New_Game()
     {
-        Debug.Log("This is the options?");
+        SceneManager.LoadScene("Scene_Name");
     }
 
     public void Instructions()
     {
+        Instruction.SetActive(true);
         Debug.Log("Instructions go here");
+    }
+
+    public void CloseInstructions()
+    {
+        Instruction.SetActive(false);
+        UI_canvas.enabled = true;
     }
 
     public void Quit()
